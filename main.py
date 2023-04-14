a556d1e7fb26487743350d75c95893e157a1f0fb
David Boesner <david.boesner@gmail.com>
2023-04-14 15:44:52 +0200

    Print ideal data

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ df.to_sql(table_name, engine, if_exists='replace', index=False)
 source = ColumnDataSource(pd.read_sql_table('training_data', engine))
 
 # Create a Bokeh plot
-
-toPlot = []
+training_data_to_plot = []
 training_data_color = ["red","green","blue","purple","yellow"]
 for i in range(1,5):
     num = i
@@ -34,10 +33,31 @@ for i in range(1,5):
     globals()[var_name] = figure(x_axis_label='X-Axis', y_axis_label='Y-Axis', title="Training data #" + str(i))
     
     globals()[var_name].line(x='x', y='y'+str(i), source=source, line_color=training_data_color[i-1])
-    toPlot.append(globals()[var_name])
+    training_data_to_plot.append(globals()[var_name])
+
+
+# load training data
+df = pd.read_csv('resources/ideal.csv')
+table_name = 'ideal_data'
+df.to_sql(table_name, engine, if_exists='replace', index=False)
+
+print (df) 
+# Create a ColumnDataSource from the data
+source = ColumnDataSource(pd.read_sql_table('ideal_data', engine))
+
+# Create a Bokeh plot
+ideal_data_to_plot = []
+for i in range(1,51):
+    num = i
+    var_name = 'var_ideal_' + str(num)
+    globals()[var_name] = figure(x_axis_label='X-Axis', y_axis_label='Y-Axis', title="Ideal data #" + str(i))
+    
+    globals()[var_name].line(x='x', y='y'+str(i), source=source)
+    ideal_data_to_plot.append(globals()[var_name])
    
-print(toPlot)
-grid = gridplot([toPlot])
+print(training_data_to_plot)
+
+grid = gridplot([training_data_to_plot, ideal_data_to_plot])
 
 # Render the plot
 show(grid)
